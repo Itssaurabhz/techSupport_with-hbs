27366fa805985d86511e11932dc281cedf6165bc
Aakash kumar <76447347+itsAakashz@users.noreply.github.com>
2023-07-12 14:09:38 +0530

    new relarease

---
+++ app.js
@@ -0,0 +1,40 @@
+const express= require("express")
+const app=express()
+const port = process.env.PORT || 80
+const hbs = require('express-hbs');
+
+//setting views engine as hbs
+app.engine('hbs', hbs.express4({
+  partialsDir: __dirname + '/views/partials'
+})); 
+   //for rendering css from public dir....     
+app.use(express.static('public'));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+
+
+
+
+//routing starts here------------------------------->
+
+//home page      
+app.get('/', (req, res) => {
+  res.render('index') 
+}) 
+
+//weather page
+app.get('/Weather', (req, res) => {
+  res.render('weather') 
+})
+                 
+//The 404 Route (ALWAYS Keep this as the last route)
+app.get('*', function(req, res){
+  res.render('404') ;
+});
+
+
+//port listening here
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+}) 
---
+++ routes/handlers.js
@@ -0,0 +1,16 @@
+// const routes =require('../app')
+
+// //home page      
+// routes.get('/', (req, res) => {
+//     res.render('index') 
+//   }) 
+  
+//   //weatheroutes
+//   routes.get('/Weather', (req, res) => {
+//     res.render('weather') 
+//   })
+                   
+//   //The 404 Route (ALWAYS Keep this as the last route)
+//   routes.get('*', function(req, res){
+//     res.render('404') ;
+//   });s
